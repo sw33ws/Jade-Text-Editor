c1f1a259432bac5a22435889c15bd7d0908fade4
Seb <gillisl22@outlook.com>
2022-07-18 02:09:42 -0400

    working on local drive, added sw.js and changed some stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "J.A.T.E",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "J.A.T.E, Text Editor",
+        short_name: "J.A.T.E",
+        description: "A web editor!",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ],
+      }),
     ],
 
     module: {
